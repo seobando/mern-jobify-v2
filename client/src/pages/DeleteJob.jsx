3b8aa3fa9a587f9a96213bfb83503192ad6358c5
Sebastian OM <sebastianobandomorales@gmail.com>
2023-10-22 21:03:05 -0500

    debugging

--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 import customFetch from "../utils/customFetch";
-import {redirect} from "reeact-router-dom";
+import {redirect} from "react-router-dom";
 
 export const action = async({params})=>{
   try{
